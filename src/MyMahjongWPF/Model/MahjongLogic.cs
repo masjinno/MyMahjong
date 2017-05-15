9cb2ea2a17754e9e0306df02b41e13d8056f26e5
masjinno <masjinno@gmail.com>
2017-05-15 13:00:50 +0900

    #1 ロジック用のクラスおよびロジックに関するユーティリティクラスを追加 (未実装)

---
+++ src/MyMahjongWPF/Model/MahjongLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMahjongWPF.Model
+{
+    /// <summary>
+    /// 麻雀のロジックに関する定義
+    /// </summary>
+    public class MahjongLogic
+    {
+    }
+}
---
+++ src/MyMahjongWPF/Model/MahjongLogicUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMahjongWPF.Model
+{
+    /// <summary>
+    /// 麻雀ロジックに関するユーティリティ
+    /// </summary>
+    public static class MahjongLogicUtility
+    {
+    }
+}
