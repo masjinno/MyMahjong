8922f4768db613c1198b5f4337b2b2065074810e
masjinno <masjinno@gmail.com>
2017-05-21 22:06:28 +0900

    #1 和了判定メソッドと国士無双判定メソッドを少し実装

--- src/MyMahjong/MahjongLogicUtility.cs
+++ src/MyMahjong/MahjongLogicUtility.cs
@@ -33,6 +33,43 @@ namespace MyMahjong
         /// <returns>和了か  true:和了  false:和了でない</returns>
         public static bool IsWonHand(Tile[] concealedTiles, TileSet[] openedSets, Tile drawnTile)
         {
+            /// 確定面子の妥当性チェック
+            foreach (TileSet ts in openedSets)
+            {
+                /// 確定面子が対子はありえない
+                if (ts.Kind == TileSet.Kinds.Pair)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(openedSets), string.Format("Argument {0} must not be Pair.", nameof(openedSets)));
+                }
+            }
+
+            /// ソート
+            Array.Sort(concealedTiles, (a, b) => a.Index - b.Index);
+
+            /// 牌数チェック
+            int nTiles = 0;
+            nTiles += concealedTiles.Count() + openedSets.Count() * 3 + 1;
+            if (nTiles != 14) throw new ArgumentException("The number of tiles is invalid.");
+
+            // 未実装
+
+            return false;
+        }
+
+
+        /// <summary>
+        /// 国士無双が揃っているか判定する
+        /// </summary>
+        /// <param name="concealedTiles">門前手牌</param>
+        /// <returns>国士無双が揃っているか  true:揃っている  false:揃っていない</returns>
+        private static bool IsThirteenOrphans(Tile[] concealedTiles)
+        {
+            /// 引数妥当性チェック
+            if (concealedTiles.Count() != 14)
+            {
+                throw new ArgumentException(string.Format("Argument {0} is invalid.", nameof(concealedTiles), nameof(concealedTiles));
+            }
+
             // 未実装
 
             return false;
