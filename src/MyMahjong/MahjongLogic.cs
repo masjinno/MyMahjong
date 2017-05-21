ec5b7126b139b19a00d1e2d5077c596bfdc27fba
masjinno <masjinno@gmail.com>
2017-05-21 13:37:06 +0900

    #1 ロジック関係クラスをMyMahjongプロジェクトに移動

---
+++ src/MyMahjong/MahjongLogic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMahjong
+{
+    /// <summary>
+    /// 麻雀のロジックに関する定義
+    /// </summary>
+    public class MahjongLogic
+    {
+        /// <summary>
+        /// 風の情報
+        /// </summary>
+        public enum WindKinds
+        {
+            East,
+            South,
+            West,
+            North
+        }
+    }
+}
--- src/MyMahjong/MahjongLogicUtility.cs
+++ src/MyMahjong/MahjongLogicUtility.cs
@@ -1,22 +1,21 @@
-﻿using MyMahjongWPF.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyMahjongWPF.Model
+namespace MyMahjong
 {
     /// <summary>
     /// 麻雀ロジックに関するユーティリティ
     /// </summary>
-    public static class MahjongLogicUtility
+    public class MahjongLogicUtility
     {
         /// <summary>
         /// 聴牌判定
         /// </summary>
         /// <returns></returns>
-        public static bool IsWaitingHand(Model.MahjongLogic.TileInfo[] hands)
+        public static bool IsWaitingHand(Tile[] concealedHands, Tile[] openedHands)
         {
             // 未実装
 
