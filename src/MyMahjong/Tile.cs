2ecca281ced09975fd33b3d5b0f2001ea5724cf7
masjinno <masjinno@gmail.com>
2017-05-24 05:35:27 +0900

    #1 数牌の数値のプロパティを作成

--- src/MyMahjong/Tile.cs
+++ src/MyMahjong/Tile.cs
@@ -105,24 +105,22 @@ namespace MyMahjong
 
         /// <summary>三元牌か</summary>
         public bool IsDragon { get { return (kind2IndexMap[Kinds.WhiteDragon] <= this.Index && this.Index <= kind2IndexMap[Kinds.RedDragon]); } }
+
+        /// <summary>
+        /// 数牌の値を取得する
+        /// </summary>
+        public int Number
         {
             get
             {
-                Dictionary<Tile.Kinds, int> kind2IndexMap = new Dictionary<Kinds, int>()
+                if (!this.IsSuit)
                 {
-                    { Kinds.Character1, 0 }, { Kinds.Character2, 1 }, { Kinds.Character3, 2 },
-                    { Kinds.Character4, 3 }, { Kinds.Character5, 4 }, { Kinds.Character6, 5 },
-                    { Kinds.Character7, 6 }, { Kinds.Character8, 7 }, { Kinds.Character9, 8 },
-                    { Kinds.Circle1, 9 }, { Kinds.Circle2, 10 }, { Kinds.Circle3, 11 },
-                    { Kinds.Circle4, 12 }, { Kinds.Circle5, 13 }, { Kinds.Circle6, 14 },
-                    { Kinds.Circle7, 15 }, { Kinds.Circle8, 16 }, { Kinds.Circle9, 17 },
-                    { Kinds.Bamboo1, 18 }, { Kinds.Bamboo2, 19 }, { Kinds.Bamboo3, 20 },
-                    { Kinds.Bamboo4, 21 }, { Kinds.Bamboo5, 22 }, { Kinds.Bamboo6, 23 },
-                    { Kinds.Bamboo7, 24 }, { Kinds.Bamboo8, 25 }, { Kinds.Bamboo9, 26 },
-                    { Kinds.East, 27 }, { Kinds.South, 28 }, { Kinds.West, 29 }, { Kinds.North, 30 },
-                    { Kinds.WhiteDragon, 31 }, { Kinds.GreenDragon, 32 }, { Kinds.RedDragon, 33 }
-                };
-                return kind2IndexMap[this.Kind];
+                    throw new InvalidOperationException("Tile must be SuitTile.");
+                }
+                if (IsCharacter) return this.Index - kind2IndexMap[Kinds.Character1] + 1;
+                else if (IsCircle) return this.Index - kind2IndexMap[Kinds.Circle1] + 1;
+                else if (IsBamboo) return this.Index - kind2IndexMap[Kinds.Bamboo1] + 1;
+                else throw new InvalidOperationException("Kind is invalid.");
             }
         }
 
