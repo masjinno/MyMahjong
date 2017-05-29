e5f2c6cd47958afb9f58ea350af17802329d8e84
masjinno <masjinno@gmail.com>
2017-05-30 00:06:17 +0900

    #3 MainWindowをViewに移動

--- src/HandCheckToolWPF/App.xaml.cs
+++ src/HandCheckToolWPF/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HandCheckToolWPF.View;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,12 @@ namespace HandCheckToolWPF
     /// </summary>
     public partial class App : Application
     {
+        private View.MainWindow view;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            view = new View.MainWindow();
+            view.Show();
+        }
     }
 }
